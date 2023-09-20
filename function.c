8b64773cd1ab2ca4170a2fd1021f2264f0bc1651
achrafelaffas <achrafelaffas@gmail.com>
2023-09-20 12:11:36 -0700

    0x19. C - Stacks, Queues - LIFO, FIFO

--- function.c
+++ function.c
@@ -1,10 +1,22 @@
 #include "monty.h"
 
 /**
- * print_top
+ * nop - Does nothing.
  *
- * @node:
- * @line:
+ * @node: the top node.
+ * @line: line number of the opcode.
+ */
+void nop(stack_t **node, unsigned int line)
+{
+	(void)node;
+	(void)line;
+}
+
+/**
+ * print_top - Prints the top node of the stack.
+ *
+ * @node: the top node.
+ * @line: line number of the opcode.
  */
 void print_top(stack_t **node, unsigned int line)
 {
@@ -20,10 +32,10 @@ void print_top(stack_t **node, unsigned int line)
 }
 
 /**
- * push_stack
+ * push_stack - Adds a node to the stack front.
  *
- * @node:
- * @line:
+ * @node: the new node.
+ * @line: line number of the opcode.
  */
 void push_stack(stack_t **node, unsigned int line)
 {
@@ -40,58 +52,45 @@ void push_stack(stack_t **node, unsigned int line)
 }
 
 /**
- * print_stack
+ * print_stack - print a doubly linked lists content
  *
  *
- * @node:
- * @line:
+ * @node: the head of the list to print
+ * @line: line number of the opcode.
  */
 
 void print_stack(stack_t **node, unsigned int line)
 {
-	stack_t *temp = *node;
+	stack_t *tempp = *node;
 
 	(void)line;
 	if (node == NULL)
 		exit(EXIT_FAILURE);
 
-	for (; temp != NULL;)
+	for (; tempp != NULL;)
 	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
+		printf("%d\n", tempp->n);
+		tempp = tempp->next;
 	}
 }
 
 /**
- * mul_top
- * @node:
- * @line:
+ * mul_top - divides the top two elements of the stack.
+ * @node: Pointer to a pointer pointing to top node.
+ * @line: line number of the opcode.
  */
 void mul_top(stack_t **node, unsigned int line)
 {
-	int mul;
+	int mull;
 
 	if (*node == NULL || (*node)->next == NULL)
 	{
 		dprintf(STDERR_FILENO, "L%u: can't mul, stack too short\n", line);
 		exit(EXIT_FAILURE);
 	}
-
 	(*node) = (*node)->next;
-	mul = (*node)->n * (*node)->prev->n;
-	(*node)->n = mul;
+	mull = (*node)->n * (*node)->prev->n;
+	(*node)->n = mull;
 	free((*node)->prev);
 	(*node)->prev = NULL;
-}
-
-/**
- * nop
- *
- * @node:
- * @line:
- */
-void nop(stack_t **node, unsigned int line)
-{
-	(void)node;
-	(void)line;
 }
